3bfc9023255dfdb97350d05e56d5344da1a9719d
Nguyễn Đông <55080498+dongnguyen1999@users.noreply.github.com>
2025-03-28 08:32:28 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,13 @@
+#!/bin/bash
+export DEBIAN_FRONTEND=noninteractive
+
+# Set default timezone to UTC (or change to Asia/Ho_Chi_Minh if preferred)
+ln -fs /usr/share/zoneinfo/UTC /etc/localtime
+dpkg-reconfigure -f noninteractive tzdata
+
 apt-get update
-apt-get install -y iproute2 openssl libuuid1 procps cifs-utils smbclient curl
+apt-get install -y iproute2 openssl libuuid1 procps cifs-utils smbclient curl tzdata
+
 curl https://cdn.kerio.com/dwn/control/control-9.4.3-8353/kerio-control-vpnclient-9.4.3-8353-linux-amd64.deb --output /tmp/kerio.deb
 cp ./install.sh /tmp/install.sh
 bash /tmp/install.sh
