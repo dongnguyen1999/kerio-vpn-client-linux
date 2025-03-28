8b3283b993669d1acd6e33e22c14b881e44b3f94
Nguyễn Đông <55080498+dongnguyen1999@users.noreply.github.com>
2025-03-28 08:39:31 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,6 +8,7 @@ dpkg-reconfigure -f noninteractive tzdata
 apt-get update
 apt-get install -y iproute2 openssl libuuid1 procps cifs-utils smbclient curl tzdata
 
+hostnamectl set-hostname kerio-ci.local
 curl https://cdn.kerio.com/dwn/control/control-9.4.3-8353/kerio-control-vpnclient-9.4.3-8353-linux-amd64.deb --output /tmp/kerio.deb
 cp ./install.sh /tmp/install.sh
 bash /tmp/install.sh
