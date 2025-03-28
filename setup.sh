08a6ea6b5f3573eccb4f11246edc3ebfc57a774a
Nguyễn Đông <55080498+dongnguyen1999@users.noreply.github.com>
2025-03-28 08:49:44 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 export DEBIAN_FRONTEND=noninteractive
 
-# --- Set a fake hostname + domain to make Kerio happy ---
+# Set fake domain name to satisfy Kerio installer
 echo "kerio-ci.local" > /etc/hostname
 hostname kerio-ci.local
-echo "127.0.0.1 kerio-ci.local localhost" >> /etc/hosts
-echo "search local" > /etc/resolv.conf
+echo "127.0.0.1 kerio-ci.local" >> /etc/hosts
 
-# Set default timezone to UTC (or change to Asia/Ho_Chi_Minh if preferred)
+# Optional: timezone
 ln -fs /usr/share/zoneinfo/UTC /etc/localtime
 dpkg-reconfigure -f noninteractive tzdata
 
+# Install dependencies
 apt-get update
 apt-get install -y iproute2 openssl libuuid1 procps cifs-utils smbclient curl tzdata
 
-hostnamectl set-hostname kerio-ci.local
+# Download and install VPN client
 curl https://cdn.kerio.com/dwn/control/control-9.4.3-8353/kerio-control-vpnclient-9.4.3-8353-linux-amd64.deb --output /tmp/kerio.deb
 cp ./install.sh /tmp/install.sh
 bash /tmp/install.sh
